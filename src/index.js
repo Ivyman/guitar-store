60a27cf9b160bdb78f2ad2e0e35d47ae295155d0
calamberia <calamberia@gmail.com>
2019-04-04 11:06:46 +0200

    fix | add basename to browser router

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import './styles/styles.sass';
 
 ReactDOM.render(
     <Provider store={store}>
-        <BrowserRouter>
+        <BrowserRouter basename={process.env.PUBLIC_URL}>
             <App />
         </BrowserRouter>
     </Provider>,
