0ddeeb4f5879c1d26084bf57d70265b94241b9b3
calamberia <calamberia@gmail.com>
2019-11-01 13:21:22 +0100

    fix | update packages

--- config-overrides.js
+++ config-overrides.js
@@ -1,8 +1,8 @@
 module.exports = function override(config, env) {
-    config.module.rules[2].oneOf[5].use[3].options = {
-        ...config.module.rules[2].oneOf[5].use[3].options,
+    // Add for loader: 'sass-loader'
+    config.module.rules[2].oneOf[5].use[4].options = {
+        ...config.module.rules[2].oneOf[5].use[4].options,
         data: `@import "src/styles/_variables.sass";`
-    }
-
+    };
     return config;
-}
+};
